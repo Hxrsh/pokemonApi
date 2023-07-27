1bc9702c859fa91f87d620fa59991b4b38233965
Hxrsh <58227294+Hxrsh@users.noreply.github.com>
2023-07-27 13:21:51 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import headimg from "./poke-head.png";
+const BASE_URL = "https://pokeapi.co/api/v2";
 
 function App() {
+  const [data, setData] = useState([]);
+  // const [dispData, setDispData] = useState([]);
+
+  const [offset, setOffest] = useState(0);
+
+  async function Fetch(offset) {
+    const url = `${BASE_URL}/pokemon?offset=${offset}&limit=10`;
+    try {
+      const response = await fetch(url);
+      const dataFetch = await response.json();
+      if (!dataFetch) return;
+      if (data.includes(dataFetch.results)) return;
+      console.log(dataFetch.results);
+      setData((prev) => {
+        return [...prev, ...dataFetch.results];
+      });
+      // return dataFetch.results;
+      // setData((prev) => {
+      //   console.log(prev, "prev");
+      //   return prev.length === 0
+      //     ? dataFetch.results
+      //     : prev.concat(dataFetch.results);
+      // });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const abortcontroller = new AbortController();
+    console.log(offset);
+    Fetch(offset);
+
+    console.log("fetched");
+  }, [offset]);
+  // useEffect(() => {
+  //   setDispData((prevState) => {
+  //     return dispData.length === 0 ? data : prevState.concat(data);
+  //   });
+  // }, [data]);
+  // useEffect(() => {
+  //   setDispData((prevState) => {
+  //     return prevState.concat(data);
+  //   });
+  //   console.log("disp data on mount", data);
+  // }, [data]);
+  // useEffect(() => {
+  //   console.log(dispData, "disp");
+  // }, [dispData]);
+  function nxtHandler() {
+    setOffest(offset + 10);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="app">
+      <div className="header">POKEMON</div>
+      <div className="card_container">
+        {data?.map((el, index) => {
+          const poke_img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            index + 1
+          }.png`;
+          return <Card onImg={poke_img} name={el.name} key={el.name}></Card>;
+        })}
+      </div>
+
+      <button onClick={nxtHandler}>Next</button>
+    </h1>
   );
 }
 
+function Card({ name, onImg }) {
+  return (
+    <div className="card">
+      <img src={onImg} alt={name} className="pokemon_image" />
+      <div className="name">{name}</div>
+    </div>
+  );
+}
 export default App;
